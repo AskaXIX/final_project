530df7695b8a7f0b8798371c4d824ea79f8636cb
Aslan <aslan.aslan.14@bk.ru>
2023-09-05 12:07:41 +0600

    done result

--- main/Program.cs
+++ main/Program.cs
@@ -1,17 +1,18 @@
-﻿Console.WriteLine("Введите строки через пробел:");
-string[] inputArray = Console.ReadLine().Split();
+﻿
 
-string[] outputArray = new string[inputArray.Length];
 
-int count = 0;
-foreach (string str in inputArray)
-{
-    if (str.Length <= 3)
-    {
-        outputArray[count] = str;
-        count++;
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
 string[] finalOutputArray = new string[count];
 
 Array.Copy(outputArray, finalOutputArray, count);
