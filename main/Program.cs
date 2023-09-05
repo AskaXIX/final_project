8a35c2d193bec608561f3c4033b35c1d4afa290c
Aslan <aslan.aslan.14@bk.ru>
2023-09-05 12:05:26 +0600

    test123

--- main/Program.cs
+++ main/Program.cs
@@ -1,14 +1,21 @@
-﻿Console.WriteLine("Введите строки через пробел:");
-string[] inputArray = Console.ReadLine().Split();
+﻿// Console.WriteLine("Введите строки через пробел:");
+// string[] inputArray = Console.ReadLine().Split();
 
-string[] outputArray = new string[inputArray.Length];
+// string[] outputArray = new string[inputArray.Length];
 
-int count = 0;
-foreach (string str in inputArray)
-{
-    if (str.Length <= 3)
-    {
-        outputArray[count] = str;
-        count++;
-    }
-}
+// int count = 0;
+// foreach (string str in inputArray)
+// {
+//     if (str.Length <= 3)
+//     {
+//         outputArray[count] = str;
+//         count++;
+//     }
+// }
+int[] arr = { 1, 2, 4, 6, 7, 8, 8 };
+
+System.Console.WriteLine(arr);
+
+int[] array = new int[] { 1, 2, 3 };
+var str = string.Join(" ", array);
+Console.WriteLine(str);
