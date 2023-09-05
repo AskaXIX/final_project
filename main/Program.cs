2a06505c315316fc27ab6ce0e6f73cab7c65e1d4
Aslan <aslan.aslan.14@bk.ru>
2023-09-05 11:52:56 +0600

    created massive

--- main/Program.cs
+++ main/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿object[] mixedArray = new object[5];
+
+mixedArray[0] = "Hello";
+mixedArray[1] = 2;
+mixedArray[2] = "world";
+mixedArray[3] = ":-)";
+
+// int[] arr = {1,2,3};
+
+// int[] numbers = { 123, 22, 3222, 411, 51 };
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] == arr.Length)
+//         System.Console.WriteLine(numbers[i]);
+//     System.Console.WriteLine();
+// }
+// var mix = string.Join(" ", mixedArray);
+// Console.WriteLine(mix);
+
+
+// int[] arr = { 1, 2, 4, 6, 7, 8, 8 };
+
+// System.Console.WriteLine(arr);
+
+// int[] array = new int[] { 1, 2, 3 };
+// var str = string.Join(" ", array);
+// Console.WriteLine(str);
+
+// int[] arr = {123, 22, 3222, 411, 51};
+// int ar = arr[0];
+// int sd = ar.
+
+// public static int StringLength(string input)
+//         {
+//             int counter = 0;
+//             if (input is null)
+//             {
+//                 return 0;
+//             }
+ 
+//             foreach (var c in input)
+//             {
+//                 //Console.WriteLine(c);
+//                 counter++;
+//             }
+ 
+//             //Console.WriteLine($"The following string has {counter} characters in it.");
+//             //Console.WriteLine($"{input}");
+//             return counter;
+//         }
